a43526955b49d37bbf55ecdc4ad6027427b69574
Swaraj12 <swarajbhalerao@gmail.com>
2024-09-19 11:39:13 -0400

    comments added

--- Triangle_problem.py
+++ Triangle_problem.py
@@ -6,15 +6,15 @@ def classify_triangle(a, b, c):
     right_triangle = sides[0]**2 + sides[1]**2 == sides[2]**2 # Check if it is a right triangle
     
     if a == b == c:
-        return "Equilateral and Right Triangle" if right_triangle else "Equilateral Triangle"
+        return "Equilateral and Right Triangle" if right_triangle else "Equilateral Triangle" #Check equilateral triangle
     elif a == b or b == c or a == c:
-        return "Isosceles and Right Triangle" if right_triangle else "Isosceles Triangle"
+        return "Isosceles and Right Triangle" if right_triangle else "Isosceles Triangle" #Check Isosceles triangle
     else:
-        return "Scalene and Right Triangle" if right_triangle else "Scalene Triangle"
+        return "Scalene and Right Triangle" if right_triangle else "Scalene Triangle" #Check scalene triangle
 
 if __name__ == "__main__":
     try:
         a, b, c = map(float, input("Enter the lengths of sides a, b, and c separated by spaces: ").split())
         print(f"The triangle is: {classify_triangle(a, b, c)}")
     except ValueError:
-        print("Please enter valid numbers.")
+        print("Please enter valid numbers.")  #Error Handling
